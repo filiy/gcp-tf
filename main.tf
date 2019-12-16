24b760e577f6aedbe529746814cc0747eda9cf56
Phil Thomson <thomson.p@gmail.com>
2019-12-15 18:30:38 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  backend "remote" {
-    hostname = "tfe.filiy.team.arctiq.ca" 
-    token = "e6r1GOPwYNzGRQ.atlasv1.IBYVeizznQBRqBea5Y3y1LqzqBH4LwUYbFr1vHO7ZGLJdwsc7m7Ap0WHlyvorLXdyoI"
-    organization = "arctiq-lab"
-
-    workspaces {
-      name = "gcp-vm"
-    }
-  }
-}
-
-
 resource "google_compute_instance" "demo" {
     name = "${var.instance_name}"
     machine_type = "${var.machine_type}"

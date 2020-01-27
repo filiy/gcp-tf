aefd2f21a23921aa143dcded54aa0d4bab0b0fb4
filiy <phil.thomson@arctiq.ca>
2020-01-27 09:38:30 -0800

    updates

---
+++ iap_test.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "remote" {
+    organization = "phil-test"
+    token = "WTBi9oUzokQUEw.atlasv1.UOgk2q1YFvFLzYIvTyJYokK24DBE8cXqB3oCd2FfzCgWDWQ2KJkVaIzG6jSMcvhCoFs"
+    workspaces {
+      name = "iap-test"
+    }
+  }
+}
+
+
+provider "google" {
+}
+
+resource "google_iap_web_iam_binding" "editor" {
+  project = "p-teckresources-tfe"
+  role = "roles/iap.httpsResourceAccessor"
+  members = [
+    "user:phil@arctiq.ca",
+  ]
+}
